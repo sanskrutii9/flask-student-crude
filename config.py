707c12906808e35db4a81bd08013cad68b2d93d2
sanskrutii9 <sanskrutiwakle1242@gmail.com>
2026-08-10 16:51:24 +0530

    change in heading

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Reads values from .env file
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback-secret-key'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///students.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "fallback-secret-key"
+
+    if os.environ.get("RAILWAY_ENVIRONMENT"):
+        SQLALCHEMY_DATABASE_URI = "sqlite:////data/students.db"
+    else:
+        SQLALCHEMY_DATABASE_URI = "sqlite:///students.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
