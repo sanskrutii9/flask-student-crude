9529a67146bc428ef24fe6779972c17d3a9b7c14
sanskrutii9 <sanskrutiwakle1242@gmail.com>
2026-08-10 17:12:24 +0530

    imported sql alchemy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from config import Config
 
-
+from flask_sqlalchemy import SQLAlchemy
 from models import db, Student
 
 app = Flask(__name__)
