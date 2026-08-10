d5dedd72c7cdaca1bf8c1a47fe550cfcd77acbe6
sanskrutii9 <sanskrutiwakle1242@gmail.com>
2026-08-10 17:10:57 +0530

    removed vague files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from config import Config
-from extensions import db # <-- Add this missing import
+
 
 from models import db, Student
 
